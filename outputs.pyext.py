6fb3b4daf9c7ebca53016a1a553174831faca594
Avinash2714 <163942768+Avinash2714@users.noreply.github.com>
2025-06-26 05:22:38 +0530

    Add files via upload

---
+++ outputs.pyext.py
@@ -0,0 +1,189 @@
+Python 3.7.6 (tags/v3.7.6:43364a7ae0, Dec 19 2019, 00:42:30) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+========================================================== RESTART: C:/Users/avina/OneDrive/Desktop/DOWNLOADS/ext AI/ai program.py =========================================================
+1 3 0
+6 8 4
+7 5 2
+-----
+1 3 4
+6 8 0
+7 5 2
+-----
+1 3 4
+6 8 2
+7 5 0
+-----
+1 3 4
+6 8 2
+7 0 5
+-----
+1 3 4
+6 0 2
+7 8 5
+-----
+1 3 4
+0 6 2
+7 8 5
+-----
+1 3 4
+7 6 2
+0 8 5
+-----
+1 3 4
+7 6 2
+8 0 5
+-----
+1 3 4
+7 0 2
+8 6 5
+-----
+1 3 4
+7 2 0
+8 6 5
+-----
+1 3 0
+7 2 4
+8 6 5
+-----
+1 0 3
+7 2 4
+8 6 5
+-----
+1 2 3
+7 0 4
+8 6 5
+-----
+1 2 3
+7 4 0
+8 6 5
+-----
+1 2 3
+7 4 5
+8 6 0
+-----
+1 2 3
+7 4 5
+8 0 6
+-----
+1 2 3
+7 4 5
+0 8 6
+-----
+1 2 3
+0 4 5
+7 8 6
+-----
+1 2 3
+4 0 5
+7 8 6
+-----
+1 2 3
+4 5 0
+7 8 6
+-----
+1 2 3
+4 5 6
+7 8 0
+-----
+Solved in 20 moves.
+>>> 
+============================================================ RESTART: C:/Users/avina/OneDrive/Desktop/DOWNLOADS/ext AI/prog8.py ============================================================
+1 3 0
+6 8 4
+7 5 2
+-----
+1 3 4
+6 8 0
+7 5 2
+-----
+1 3 4
+6 8 2
+7 5 0
+-----
+1 3 4
+6 8 2
+7 0 5
+-----
+1 3 4
+6 0 2
+7 8 5
+-----
+1 3 4
+0 6 2
+7 8 5
+-----
+1 3 4
+7 6 2
+0 8 5
+-----
+1 3 4
+7 6 2
+8 0 5
+-----
+1 3 4
+7 0 2
+8 6 5
+-----
+1 3 4
+7 2 0
+8 6 5
+-----
+1 3 0
+7 2 4
+8 6 5
+-----
+1 0 3
+7 2 4
+8 6 5
+-----
+1 2 3
+7 0 4
+8 6 5
+-----
+1 2 3
+7 4 0
+8 6 5
+-----
+1 2 3
+7 4 5
+8 6 0
+-----
+1 2 3
+7 4 5
+8 0 6
+-----
+1 2 3
+7 4 5
+0 8 6
+-----
+1 2 3
+0 4 5
+7 8 6
+-----
+1 2 3
+4 0 5
+7 8 6
+-----
+1 2 3
+4 5 0
+7 8 6
+-----
+1 2 3
+4 5 6
+7 8 0
+-----
+Solved in 20 moves.
+>>> 
+============================================================ RESTART: C:/Users/avina/OneDrive/Desktop/DOWNLOADS/ext AI/prog8.py ============================================================
+
+Initial state is ('Far-Chair', 'Chair-Not-Under-Banana', 'Off-Chair', 'Empty')
+
+✅ Solution Found!
+Actions to achieve goal:
+Move to Chair → ('Near-Chair', 'Chair-Not-Under-Banana', 'Off-Chair', 'Empty')
+Push Chair under Banana → ('Near-Chair', 'Chair-Under-Banana', 'Off-Chair', 'Empty')
+Climb Chair → ('Near-Chair', 'Chair-Under-Banana', 'On-Chair', 'Empty')
+Grasp Banana → ('Near-Chair', 'Chair-Under-Banana', 'On-Chair', 'Holding')
+Final State: ('Near-Chair', 'Chair-Under-Banana', 'On-Chair', 'Holding')
+>>> 
